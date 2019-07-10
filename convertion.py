e8a700f0836ba0f01e81d92d27d6435e49ffbd18
JohnnieJnr <52750509+JohnnieJnr@users.noreply.github.com>
2019-07-10 14:22:47 +0000

    Add files via upload

---
+++ convertion.py
@@ -0,0 +1,17 @@
+import math
+pi = math.pi
+degree = 32
+radian = degree * (pi / 180)
+print(radian)
+
+import math
+r = 'radius'
+pi = math.pi
+r = int(input("radius: "))
+suf_area = 4 * pi * (r * r)
+volume = 4 / 3 * pi * math.pow(r, 3)
+print("surface area: ")
+print(suf_area)
+print ("")
+print("volume: ")
+print(volume)
---
+++ functions.py
@@ -0,0 +1,26 @@
+def star():
+    man =  ["****", "****", "****", "****"]
+    for man in man:
+        print (man)   
+star()
+    
+#
+rows = 4
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print("\r")
+        
+for i in range (rows, 0, -1):
+    for j in range(0, i -1):
+        print("*", end=' ')
+    print("\r")
+        
+#         
+print("*",)
+print("**")
+print("***")
+print("****")
+print("***")
+print("**")
+print("*")
